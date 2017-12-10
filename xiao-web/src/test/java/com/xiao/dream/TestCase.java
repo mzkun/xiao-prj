8c660b5ff8406f081e4d3ea8b110a18cca9574f9
mzkun <mzkuncool@163.com>
2017-12-10 14:28:38 +0800

    提交测试类

---
+++ xiao-web/src/test/java/com/xiao/dream/TestCase.java
@@ -0,0 +1,13 @@
+package com.xiao.dream;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by dream on 17/12/10.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:spring"})
+public class TestCase {
+}
